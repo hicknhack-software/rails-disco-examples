f461e7fc308639429b493cc33b1cf842b0af7de1
Robert Kranz <robert.kranz@hicknhack-software.com>
2013-09-19 11:24:15 +0200

    * new migration and unique id generation for example

--- disco_blog/domain/command_processors/domain/post_processor.rb
+++ disco_blog/domain/command_processors/domain/post_processor.rb
@@ -11,6 +11,7 @@ module Domain
     end
 
     process PostCreateCommand do |command|
+      command.id = ActiveDomain::UniqueCommandIdRepository.new_for command.class.name
       command.is_valid_do { event PostCreateEvent.new command.to_hash }
     end
   end
