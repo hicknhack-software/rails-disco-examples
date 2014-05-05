77eeea8840e8b53c114ab845e991844522904458
Robert Kranz <robert.kranz@hicknhack-software.com>
2014-05-05 14:24:49 +0200

    * fixed tests

--- disco_blog/test/projections/posts_projection_test.rb
+++ disco_blog/test/projections/posts_projection_test.rb
@@ -8,13 +8,13 @@ class PostsProjectionTest < ActiveSupport::TestCase
 
   test "should create post" do
     assert_difference('Post.count') do
-      @projection.post_create_event(PostCreateEvent.new({id: 1000, title: 'test', text: 'test'}))
+      @projection.created_post_event(CreatedPostEvent.new({id: 1000, title: 'test', text: 'test'}))
     end
 
   end
 
   test "should update post" do
-    @projection.post_update_event(PostUpdateEvent.new({id: @post.id, title: 'test', text: 'test'}))
+    @projection.updated_post_event(UpdatedPostEvent.new({id: @post.id, title: 'test', text: 'test'}))
     updated_post = Post.find(@post.id)
     assert_equal 'test', updated_post.title
     assert_equal 'test', updated_post.text
@@ -22,7 +22,7 @@ class PostsProjectionTest < ActiveSupport::TestCase
 
   test "should destroy post" do
     assert_difference('Post.count', -1) do
-      @projection.post_delete_event(PostDeleteEvent.new({id: @post.id}))
+      @projection.deleted_post_event(DeletedPostEvent.new({id: @post.id}))
     end
   end
 end
