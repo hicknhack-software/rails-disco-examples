ead6c6e3bc95f7c792dbaa5dbcdc73d0066d079f
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2015-03-10 17:06:24 +0100

    simplified PostProcessor

--- disco_blog/domain/command_processors/domain/post_processor.rb
+++ disco_blog/domain/command_processors/domain/post_processor.rb
@@ -2,19 +2,19 @@ module Domain
   class PostProcessor
     include ActiveDomain::CommandProcessor
     process DeletePostCommand do |command|
-      command.is_valid_do do
+      if command.valid?
         event DeletedPostEvent.new command.to_hash
       end
     end
 
     process UpdatePostCommand do |command|
-      command.is_valid_do do
+      if command.valid?
         event UpdatedPostEvent.new command.to_hash
       end
     end
 
     process CreatePostCommand do |command|
-      command.is_valid_do do
+      if command.valid?
         id = ActiveDomain::UniqueCommandIdRepository.new_for command.class.name
         event CreatedPostEvent.new command.to_hash.merge(id: id)
       end
